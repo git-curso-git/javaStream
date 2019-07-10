9a34a4da890bbd9889c0ae80d547629cad36020f
GIT CURSO <git.curso.git@gmail.com>
2019-07-10 19:46:08 +0200

    save work

--- Java8Stream/src/eci/seguros/webs/Aplicacion.java
+++ Java8Stream/src/eci/seguros/webs/Aplicacion.java
@@ -1,15 +1,22 @@
 package eci.seguros.webs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication // ya incluye @EnableAutoConfiguration
 public class Aplicacion {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Aplicacion.class);
 
 	public static void main(String[] args) {
 		System.out.format("%-20s : %06x\n", "Hola, soy yo", 155);
 		System.out.format("%20s y a continuaci�n un n�mero %d\n", "Hola, soy yo", 155);
 		
+		logger.info("arrancando spring boot");
 		SpringApplication.run(Aplicacion.class, args);
 	}
 }
--- Java8Stream/src/eci/seguros/webs/SiniestroController.java
+++ Java8Stream/src/eci/seguros/webs/SiniestroController.java
@@ -1,14 +1,20 @@
 package eci.seguros.webs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SiniestroController {
 
+	private static final Logger logger = LoggerFactory.getLogger(SiniestroController.class);
+	
     @RequestMapping("/list")
     public String listSiniestro() {
-        return "Soy el endpoint listSiniestro desde la url 'list'!";
+    	logger.trace("Endpoint listSiniestro");
+		String strResult = "Hola, soy el endpoint listSiniestro desde la url 'list'!";
+        return strResult;
     }
 
 }
