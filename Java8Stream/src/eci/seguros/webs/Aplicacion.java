2250c905330ed721153b8356996cd3a93ba7cfb1
GIT CURSO <git.curso.git@gmail.com>
2019-07-10 19:04:26 +0200

    actuator

--- Java8Stream/src/eci/seguros/webs/Aplicacion.java
+++ Java8Stream/src/eci/seguros/webs/Aplicacion.java
@@ -1,11 +1,9 @@
 package eci.seguros.webs;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication // ya incluye @EnableAutoConfiguration
 public class Aplicacion {
 
 	public static void main(String[] args) {
