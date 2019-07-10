d36393c0d1ca01500ce2aff97410f857a4b1cb21
GIT CURSO <git.curso.git@gmail.com>
2019-07-10 18:56:49 +0200

    autoconfiguration

--- Java8Stream/src/eci/seguros/webs/Aplicacion.java
+++ Java8Stream/src/eci/seguros/webs/Aplicacion.java
@@ -1,9 +1,11 @@
 package eci.seguros.webs;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Aplicacion {
 
 	public static void main(String[] args) {
